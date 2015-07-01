fd0b5a429c7c5f9c6a773308237237a71d324324
Michael <michaelrobert.fs@gmail.com>
2015-07-01 18:02:46 -0400

    Second iteration.  Print out stars for rating.

--- display.js
+++ display.js
@@ -5,9 +5,19 @@ function display(episodes){
 for(var i = 0; i < episodes.length; i++){
 console.log("Title:       " + episodes[i].title + "      Episode" + episodes[i].episode_number);
 console.log("Description: " + episodes[i].description);
-console.log("Rating       " + episodes[i].rating);
-console.log("**********************************************\n\n")
+console.log("Rating       " + episodes[i].rating + " "  + starRating(episodes[i].rating));
+console.log("**********************************************\n\n");
 	}
 
+}
+
+function starRating(rating){
+	numStars = (10 * rating) % 10;
+		starStr = "";
+
+	for(var i = 0; i < numStars; i++){
+		starStr += "*";	
+	}
+	return starStr;
 }
 module.exports = display
